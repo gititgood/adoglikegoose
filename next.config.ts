c47306ee83e7a04452e2fea279d363baa9ce1b88
gititgood <nvl789@gmail.com>
2026-08-16 13:39:42 -0700

    clean configuration file

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,10 @@
+// @ts-nocheck
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* Your existing template config options like images or sanity hooks go here */
-  
   typescript: {
-    // This stops TypeScript from blocking your build
     ignoreBuildErrors: true,
-  }, 
+  },
 };
 
 export default nextConfig;
